70b52b5e2499dd31f02337dacb5691be1f335d3d
Suman Kumar Sinha <110647930+SumanKS1998@users.noreply.github.com>
2022-10-30 18:08:26 +0530

    Update ServiceModal.jsx

--- components/Menu/ServiceModal.jsx
+++ components/Menu/ServiceModal.jsx
@@ -82,7 +82,7 @@ export default function ServiceModal({
               })}
             </Stack>
           ) : (
-            <Box component="img" src="/images/not-found.png" sx={{width:"60px",display:"block",mx:"auto"}}/>
+            <Box component="img" src="/images/not-found.png" sx={{width:"150px",display:"block",mx:"auto"}}/>
           )}
 
           <Button
